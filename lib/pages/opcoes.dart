da86d5b5734beebb5126a0180c02fb7081172eb0
Diego Borba <diegoborbas03@gmail.com>
2023-05-22 23:49:57 -0300

    Update opcoes.dart

--- lib/pages/opcoes.dart
+++ lib/pages/opcoes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login.pages.dart';
-import 'package:flutter_application_1/pages/transferencia.pages.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -28,134 +26,240 @@ class MyApp extends StatelessWidget {
 class Opcoes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => LoginPage()),
-            );
-            // Ação ao pressionar a seta para a esquerda
-          },
-        ),
-        title: Text(
-          'Stock Manager',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          Image.asset(
-            'assets/Stock_Manager_logo.png',
-            width: 70.0,
-            height: 70.0,
-          ),
-        ],
+    return MaterialApp(
+      // .....
+      title: 'Cards',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
       ),
-      body: Center(
-        
-        child: GridView.count(
-          crossAxisCount: 2, // Dois itens por linha
-          padding: EdgeInsets.all(20.0),
-          mainAxisSpacing: 40.0, // Espaçamento vertical entre os itens
-          crossAxisSpacing: 30.0, // Espaçamento horizontal entre os itens
-          children: [
-            ButtonWithImage(
-              image: AssetImage('assets/images/card1.png'),
-              text: 'Consulta',
-              onPressed: () {
-                // Ação ao pressionar o botão 1
-              },
-            ),
-            ButtonWithImage(
-              image: AssetImage('assets/images/card2.png'),
-              text: 'Cadastro ',
-              onPressed: () {
-                // Ação ao pressionar o botão 2
-              },
-            ),
-            ButtonWithImage(
-              image: AssetImage('assets/images/card3.png'),
-              text: 'Recebimento de Produto',
-              onPressed: () {
-                // Ação ao pressionar o botão 3
-              },
-            ),
-            ButtonWithImage(
-              image: AssetImage('assets/images/card4.png'),
-              text: 'Transferência',
-              onPressed: () {
-                Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => TransferenciaPage()),
-                );
-                // Ação ao pressionar o botão 4
-              },
-            ),
-            ButtonWithImage(
-              image: AssetImage('assets/images/card5.png'),
-              text: 'Configuração',
-              onPressed: () {
-                // Ação ao pressionar o botão 5
-              },
-            ),
-            ButtonWithImage(
-              image: AssetImage('assets/images/card6.png'),
-              text: 'Sair',
-              onPressed: () {
-                // Ação ao pressionar o botão 6
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonWithImage extends StatelessWidget {
-  final ImageProvider image;
-  final String text;
-  final VoidCallback onPressed;
-
-  const ButtonWithImage({
-    required this.image,
-    required this.text,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 200,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          primary: Color(0xffFF914D),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: image,
-              fit: BoxFit.contain,
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              text,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      home: Scaffold(
+        body: Container(
+          color: const Color(0xFF00141b),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(2.0),
+                child: Row(
+                  children: [
+                    Spacer(),
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                    ),
+                    Spacer(),
+                    Text(
+                      'Stock Manager',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    Image.asset(
+                      'assets/Stock_Manager_logo.png',
+                      width: 80.0,
+                      height: 80.0,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 2.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card1.png',
+                            width: 110.0,
+                            height: 110.0,
+                          ),
+                          SizedBox(height: 25.0),
+                          Text(
+                            'Consulta',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 25.0),
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card2.png',
+                            width: 110.0,
+                            height: 110.0,
+                          ),
+                          SizedBox(height: 25.0),
+                          Text(
+                            'Cadastro',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card3.png',
+                            width: 105.0,
+                            height: 105.0,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Recebimento de Produto',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 25.0),
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card4.png',
+                            width: 110.0,
+                            height: 110.0,
+                          ),
+                          SizedBox(height: 25.0),
+                          Text(
+                            'Transferência',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card5.png',
+                            width: 110.0,
+                            height: 110.0,
+                          ),
+                          SizedBox(height: 25.0),
+                          Text(
+                            'Configuração',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 25.0),
+                  Expanded(
+                    child: Card(
+                      color: Color(0xffFF914D),
+                      elevation: 15.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/card6.png',
+                            width: 110.0,
+                            height: 110.0,
+                          ),
+                          SizedBox(height: 25.0),
+                          Text(
+                            'Sair',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
