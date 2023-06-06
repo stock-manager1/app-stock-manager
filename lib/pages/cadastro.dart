62943cfa74600204335c5ea22a663dbe820228d8
Luiz Felipe Rodrigues de Souza <lipelff20@gmail.com>
2023-06-06 04:50:43 -0300

    Alteração na tela de cadastro

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -6,6 +6,10 @@ import 'package:flutter_application_1/pages/transferencia_pages.dart';
 import 'package:flutter_application_1/pages/widgets_pages.dart';
 import 'package:flutter_application_1/pages/consulta_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,234 +27,184 @@ class MyApp extends StatelessWidget {
 class Cadastro extends StatelessWidget {
   final double appBarTopMargin = 0.0; // Margem superior do AppBar
   final double appBarBottomMargin = 0.0; // Margem inferior do AppBar
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize:
             Size.fromHeight(80.0 + appBarTopMargin + appBarBottomMargin),
-        child: Container(
-          margin:
-              EdgeInsets.only(top: appBarTopMargin, bottom: appBarBottomMargin),
-          child: AppBar(
-            backgroundColor: Color(0xFF00141b), // Definindo a cor preta
-            title: Text(
-              'Cadastrar',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-                color: Colors.white,
-              ),
+        child: AppBar(
+          backgroundColor: Color(0xFF00141b), // Definindo a cor preta
+          title: Text(
+            'Cadastrar',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: Colors.white,
             ),
-            centerTitle: true,
-            actions: [
-              Image.asset(
-                'assets/loogo.png',
-                width: 80.0,
-                height: 80.0,
-              ),
-            ],
           ),
+          centerTitle: true,
+          actions: [
+            Image.asset(
+              'assets/Stock_Manager_logo.png',
+              width: 80.0,
+              height: 80.0,
+            ),
+          ],
         ),
       ),
       backgroundColor: Color(0xFF00141b),
-      body: Center(
+      body: SingleChildScrollView( // Envolve as colunas em um SingleChildScrollView
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  hintText: 'Nome do Produto',
-                  hintStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                  ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Poppins',
-                  color: Colors.black,
+                hintText: 'Nome do Produto',
+                hintStyle: TextStyle(
+                  color: Colors.black45,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  hintText: 'Fornecedor',
-                  hintStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                  ),
-                ),
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Poppins',
-                  color: Colors.black,
-                ),
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                color: Colors.black,
               ),
             ),
+            SizedBox(height: 60.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                hintText: 'Fornecedor',
+                hintStyle: TextStyle(
+                  color: Colors.black45,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 60.0),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                Expanded(
+                  flex: 2, // Defina uma proporção maior para a caixa de preço
                   child: Container(
-                    width: 150,
                     height: 50,
                     color: Color(0xFF00141b),
-                    child: Center(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          hintText: 'Preço',
-                          hintStyle: TextStyle(
-                            color: Colors.black45,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                          ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: 'Poppins',
-                          color: Colors.black,
+                        hintText: 'Preço',
+                        hintStyle: TextStyle(
+                          color: Colors.black45,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
                         ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        color: Colors.black,
                       ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                SizedBox(width: 16.0),
+                Expanded(
+                  flex: 3, // Defina uma proporção menor para a caixa de quantidade de entrada
                   child: Container(
-                    width: 150,
                     height: 50,
                     color: Color(0xFF00141b),
-                    child: Center(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          hintText: 'Custo',
-                          hintStyle: TextStyle(
-                            color: Colors.black45,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                          ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: 'Poppins',
-                          color: Colors.black,
+                        hintText: 'Quantidade de Entrada',
+                        hintStyle: TextStyle(
+                          color: Colors.black45,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
                         ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        color: Colors.black,
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      hintText: 'Quantidade Estoque Depósito',
-                      hintStyle: TextStyle(
-                        color: Colors.black45,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
-                    ),
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Poppins',
-                      color: Colors.black,
-                    ),
+            SizedBox(height: 60.0),
+            TextField(
+              maxLines: 5, // Define o número máximo de linhas da caixa de texto
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                hintText: 'Descrição',
+                hintStyle: TextStyle(
+                  color: Colors.black45,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 60.0),
+            SizedBox(
+              height: 40, // Aumenta o tamanho vertical do botão de cadastro
+              width: 500,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF5271FF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      hintText: 'Descrição',
-                      hintStyle: TextStyle(
-                        color: Colors.black45,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 50.0,
-                          horizontal:
-                              10.0), // Aumenta o tamanho da caixa de texto
-                    ),
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Poppins',
-                      color: Colors.black,
-                    ),
+                child: Text(
+                  'Cadastrar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
-                SizedBox(
-                  height: 40, // Aumenta o tamanho vertical do botão de cadastro
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF5271FF),
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                    ),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
-                        print('Botão 1 pressionado!');
-                      },
-                      child: const Center(
-                        child: Text(
-                          'Cadastrar',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ],
         ),
